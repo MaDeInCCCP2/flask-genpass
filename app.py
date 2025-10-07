bbdcd9bf77f21ae207c7e25a19956632ff7c1d16
MaDeInCCCP2 <61327828+MaDeInCCCP2@users.noreply.github.com>
2025-10-07 17:29:58 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import random
 import string
 
 app = Flask(__name__)
 
+app.secret_key = '123123'
+
+USER = {
+    "username": "admin",
+    "password": "qwerty123"
+}
+
 def gen_passwordeasy():
     chars = list(string.ascii_uppercase + string.ascii_lowercase)
     res = random.sample(chars, 5)
     return ''.join(res)
-
 
 def gen_passwordmedium():
     chars = list(string.ascii_uppercase + string.ascii_lowercase + string.digits)
@@ -20,9 +26,13 @@ def gen_passwordhard():
     res = random.sample(chars, 16)
     return ''.join(res)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'username' in session:
+        return render_template('index.html')
+    else:
+        return render_template('login.html')
 
 @app.route('/generateeasy', methods=['GET'])
 def generate_easy():
@@ -38,6 +48,20 @@ def generate_medium():
 def generate_hard():
     password = gen_passwordhard()
     return render_template('index.html', password=password)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == USER['username'] and password == USER['password']:
+            session.clear()
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            flash(message='Неправильный пароль', category='error')
 
 if __name__ == '__main__':
     print(app.url_map)
